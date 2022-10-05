d4494b790815f13c4ff8c23a1c984e48f0e2aa17
salutonly <ylianggeom@gmail.com>
2022-10-05 17:12:57 +0800

    修改：     .DS_Store 	修改：     .vuepress/config.ts 	修改：     scripts/deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,18 @@
-cd ..
+#!/usr/bin/env sh
+
 # 终止一个错误
 set -e
-cd Blog
-# 生成静态资源
-vuepress build
+
+# 构建
+npm run build
 
 # 进入生成的构建文件夹
-cd vuepress
+cd ./.vuepress/public
+
 git init
 git add -A
 git commit -m 'deploy'
+
 git push -f https://github.com/salutonly/salutonly.github.io.git master
+
+cd -
